1cd2021d3071b790d962df3acc1aad578008eba0
Morella92 <morena.piem@outlook.com>
2023-03-17 16:16:51 +0100

    START created form

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="intro.php" method="GET">
+    <input type="textarea" rows="10" cols="50" name="paragraph" placeholder="Inserisci un testo">
+    <input type="text" name="censored" placeholder="Inserisci una parola">
+    <button type="submit">Invia</button>
+</form>
